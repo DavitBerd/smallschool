11a215e34f176cc821584fb2050a0d66f296ea39
DavitBerd <davitberdzenishvili2004@gmail.com>
2025-11-11 15:28:44 +0400

    dependeciesfix

--- server/src/auth/strategies/jwt.strategy.ts
+++ server/src/auth/strategies/jwt.strategy.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
 import * as dotenv from 'dotenv';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../../users/users.service';
 
 dotenv.config();
 

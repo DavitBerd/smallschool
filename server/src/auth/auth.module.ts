c3558cac9bf6c76581c83a335909fc75180829a5
DavitBerd <davitberdzenishvili2004@gmail.com>
2025-11-11 15:18:44 +0400

    secondFix

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,7 +5,8 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
+
 
 dotenv.config();
 
